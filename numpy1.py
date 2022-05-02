175d87edade549fc132f28236bed54c92e634280
Yadnesh Dhanawade <59685501+YadneshD@users.noreply.github.com>
2022-05-02 21:39:57 +0530

    added few 1D functions

--- numpy1.py
+++ numpy1.py
@@ -1,5 +1,6 @@
 import numpy as np
-# numpy==1.22.1
+# tested on numpy==1.22.1
+# https://betterprogramming.pub/numpy-illustrated-the-visual-guide-to-numpy-3b1d4976de1d
 print(np.__version__)
 
 
@@ -9,12 +10,9 @@ ones1 = np.ones(shape=(3,2,1), dtype=int)
 print("ones1 = ", ones1)
 empty1 = np.empty(shape=(2,1, 3), dtype=int)
 print("empty1 = ", empty1)
-
 full1 = np.full(fill_value=96.3, shape=(2,1, 3), dtype=float)
 print("full1 = ", full1)
-
 print("ones1 like full1", np.ones_like(full1))
-
 print("----------------------------------------------------------------")
 print("arange", np.arange(2,18,2))
 print("linspace", np.linspace(2.0,18.0,3))
@@ -23,8 +21,8 @@ print("randint of given shape", np.random.randint(5,15,(2,1,2)))
 print("rand", np.random.rand(2,1,2)) # create array of random floating numbers 0-1 of given shape 
 print("uniform - ", np.random.uniform(5,15,4))
 print("uniform of given shape -", np.random.uniform(5,15,(1,2,1)))
-print("--------------NUMPY INDEXING---------------------------------------")
 
+print("--------------NUMPY INDEXING---------------------------------------")
 arr1 = np.arange(17, 27)
 print("arr1 =", arr1, "shape =", arr1.shape)
 print("slicing", arr1[2:6])
@@ -37,15 +35,24 @@ print("fancy indexing", arr1[[2,3,4,7,8]])
 print("---------------------REPLACING ELEMS-----------------")
 arr1[3:5] = 97
 print("replace elems of multple indices by single value", arr1)
-
 arr1[7:9] = [47, 56]
 print("replace elems of multple indices by multiple values", arr1)
 
 print("---------------------BOOLEAN OPERATIONS---------------")
-
 print("remove elems using conditions", arr1[arr1%2==1])
 print("np.any -", np.any(arr1 > 20))
 print("np.all -", np.any(arr1 < 0))
+print("AND operation", arr1[(arr1%2==0) & (arr1>=22)])
+print("OR operation", arr1[(arr1%2==0) | (arr1>=50)])
 
+print("sine pi/6, pi/4, pi/3, pi/2 -", np.sin([np.pi/6, np.pi/4, np.pi/3, np.pi/2])) # Radians
+print("Inverse sine ", np.arcsin([0.5, 0.70710678, 0.8660254, 1.0])) # Radians
+# arcsin is a multivalued function. For each x there are infinitely many numbers z such that sin(x)=z. The convention is to return the angle z whose real part lies in [-pi/2, pi/2]
+print("hypot function ", np.hypot(3*np.ones((3, 3)), 4*np.ones((3, 3))))
+# Pythogoras Theorem. Given the “legs” of a right triangle, return its hypotenuse.
 
+print("like sorted", np.sort(arr1))
+print("like sort", arr1.sort())
+print("search element in sorted array", np.searchsorted(arr1, 47))
+#In numpy, the best way search a element is to sort array first and then search it
 
